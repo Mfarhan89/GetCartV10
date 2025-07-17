40ba93031ecddbb60cd0a821a42c060f4a5218d3
Mfarhan89 <farhan782@gmail.com>
2025-07-17 13:57:15 +0000

    Fix: corrected plugin syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   plugins: [
     {
-    resolve: `gatsby-plugin-netlify-cms`,
+    resolve: 'gatsby-plugin-netlify-cms',
     options: {
       modulePath: `${__dirname}/src/cms/cms.js`,
       'gatsby-plugin-netlify',
